91bf91e6eb1a8ca9c38b8e49d4e1350f6d8e97c6
horacio9a <horacio9a@gmail.com>
2017-12-28 18:35:39 +0100

    v.1.0.7 Basic myfreecams-anonymous all modes script

--- mfc.py
+++ mfc.py
@@ -16,17 +16,18 @@ print(colored(" => START <=", "yellow", "on_blue"))
 print
 
 vs_str = {}
-vs_str[0] = "PUBLIC"
-vs_str[2] = "AWAY"
-vs_str[12] = "PRIVATE"
-vs_str[13] = "GROUP"
-vs_str[90] = "CAM-OFF"
-vs_str[127] = "OFFLINE"
+vs_str[0] = 'PUBLIC'
+vs_str[2] = 'AWAY'
+vs_str[12] = 'PRIVATE'
+vs_str[13] = 'GROUP'
+vs_str[90] = 'CAM-OFF'
+vs_str[127] = 'OFFLINE'
 
 def fc_decode_json(m):
    return json.loads(m[m.find('{'):].decode('utf-8','ignore'))
 
 def read_model_data(m):
+   global model
    global server
    global cid
    global uid
@@ -36,18 +37,18 @@ def read_model_data(m):
       sid = msg['sid']
       level = msg['lv']
    except:
-      print(colored(" => Error reply ... check your entry <=", "yellow", "on_red"))
+      print(colored(" => Error reply ... check your entry <=", "white", "on_red"))
       print(colored("\n => END <=", "yellow","on_blue"))
       time.sleep(6)
       sys.exit()
    vs = msg['vs']
    if vs == 127:
-      print (colored(" => ({}) is OFFLINE <=", "yellow", "on_red")).format(camgirl)
+      print (colored(" => ({}) is OFFLINE <=", "white", "on_red")).format(model)
       print(colored("\n => END <=", 'yellow','on_blue'))
       time.sleep(3)
       sys.exit()
 
-   usr = msg['nm']
+   model = msg['nm']
    uid = msg['uid']
    cid = msg['uid'] + 100000000
    m_info = msg['m']
@@ -110,19 +111,19 @@ def read_model_data(m):
       if flags == 15400:
          buf = '(TRUEPVT)'
       else:
-         buf = "("+vs_str[vs]+")"
+         buf = '('+vs_str[vs]+')'
    except:
       pass
 
-   print (colored(" => ({}) * {} * ({}) * Server: {} * Score: {} * Viewers: {} <=", "yellow", "on_blue")).format(camgirl,buf,cserver,server,camscore,rc)
+   print (colored(" => ({}) * {} * ({}) * Server: {} * Flags: {} * Score: {} <=", "yellow", "on_blue")).format(model,buf,cserver,server,flags,camscore)
    print (colored("\n => Continent: {} * Location: {}-{} * Age: {} * Ethnic: {} <=", "yellow", "on_blue")).format(continent,city,country,age,ethnic)
-   print (colored("\n => Occupation: {} * New model: {} * Blurb: {} <=", "yellow", "on_blue")).format(occupation,newmodel,blurb)
+   print (colored("\n => Occupation: {} * New: {} * Viewers: {} * Blurb: {} <=", "yellow", "on_blue")).format(occupation,newmodel,rc,blurb)
    print (colored("\n => Topic: {} <=\n", "yellow", "on_blue")).format(topic)
    # print (colored(" => (MODEL DATA) => {} <=\n", "white", "on_blue")).format(mdata)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
    if len(sys.argv) > 1:
-      camgirl = sys.argv[1]
+      model = sys.argv[1]
    else:
       while True:
          try:
@@ -135,9 +136,9 @@ if __name__ == "__main__":
             break
          except ValueError:
             print
-            print(colored(" => Input must be a number <=", "yellow", "on_red"))
+            print(colored(" => Input must be a number <=", "white", "on_red"))
             print
-      camgirl = open(config.get('files', 'model_list'), 'r').readlines()[cgn-1][:-1]
+      model = open(config.get('files', 'model_list'), 'r').readlines()[cgn-1][:-1]
    try:
       cservers = ["xchat107","xchat108","xchat61","xchat94","xchat109","xchat22","xchat47","xchat48","xchat49",
                  "xchat26","ychat30","ychat31","xchat95","xchat20","xchat111","xchat112","xchat113","xchat114",
@@ -161,13 +162,13 @@ if __name__ == "__main__":
       # FCTYPE_PING = 1
       send_msg_ping = '1 0 0 0 0\n\0'
       # FCTYPE_MODEL = 10
-      send_msg_model = '10 0 0 20 0 {}\n\0'.format(camgirl)
+      send_msg_model = '10 0 0 20 0 {}\n\0'.format(model)
       # FCTYPE_LOGOUT = 99
       send_msg_logout = '99 0 0 0 0'
       ws.send(send_msg_hello)
       ws.send(send_msg_login)
    except:
-      print (colored(" => {} server is busy ... Try again <=", "yellow", "on_red")).format(cserver)
+      print (colored(" => {} server is busy ... Try again <=", "white", "on_red")).format(cserver)
       print(colored("\n => END <=", "yellow","on_blue"))
       time.sleep(6)
       sys.exit()
@@ -178,7 +179,7 @@ if __name__ == "__main__":
       sock_buf = rembuf+sock_buf
       rembuf = ''
       while True:
-         hdr = re.search (r"(\w+) (\w+) (\w+) (\w+) (\w+)", sock_buf)
+         hdr = re.search (r'(\w+) (\w+) (\w+) (\w+) (\w+)', sock_buf)
          if bool(hdr) == 0:
             break
          fc = hdr.group(1)
@@ -207,7 +208,7 @@ if __name__ == "__main__":
                 mode = int(raw_input(colored(" => Mode => Exit(6) => URL(5) => YTDL(4) => SL(3) => LS(2) => FFMPEG(1) => FFPLAY(0) => ", "white", "on_green")))
                 break
              except ValueError:
-                print(colored("\n => Input must be a number <=\n", "yellow", "on_red"))
+                print(colored("\n => Input must be a number <=\n", "white", "on_red"))
          if mode == 0:
             mod = 'FFPLAY'
          if mode == 1:
@@ -224,14 +225,14 @@ if __name__ == "__main__":
             mod = 'EXIT'
 
          hlsurl = 'http://video{}.myfreecams.com:1935/NxServer/ngrp:mfc_{}.f4v_mobile/playlist.m3u8'.format(server,cid)
-         timestamp = str(time.strftime("%d%m%Y-%H%M%S"))
-         stime = str(time.strftime("%H:%M:%S"))
+         timestamp = str(time.strftime('%d%m%Y-%H%M%S'))
+         stime = str(time.strftime('%H:%M:%S'))
          path = config.get('folders', 'output_folder')
-         fn = camgirl + '_MFC_' + timestamp
-         fn1 = camgirl + '_MFC_' + timestamp + '.flv'
-         fn2 = camgirl + '_MFC_' + timestamp + '.mp4'
-         fn3 = camgirl + '_MFC_' + timestamp + '.ts'
-         fn4 = camgirl + '_MFC_' + timestamp + '.txt'
+         fn = model + '_MFC_' + timestamp
+         fn1 = model + '_MFC_' + timestamp + '.flv'
+         fn2 = model + '_MFC_' + timestamp + '.mp4'
+         fn3 = model + '_MFC_' + timestamp + '.ts'
+         fn4 = model + '_MFC_' + timestamp + '.txt'
          pf1 = (path + fn1)
          pf2 = (path + fn2)
          pf3 = (path + fn3)
@@ -244,7 +245,7 @@ if __name__ == "__main__":
 
          if mod == 'FFPLAY':
             print (colored("\n => FFPLAY => {} <=", "yellow", "on_magenta")).format(fn)
-            command = '{} -hide_banner -loglevel panic -i {} -infbuf -autoexit -x 640 -y 480 -window_title "{} * {} * {} * {}"'.format(ffplay,hlsurl,camgirl,stime,cserver,cgn)
+            command = '{} -hide_banner -loglevel panic -i {} -infbuf -autoexit -x 640 -y 480 -window_title "{} * {} * {} * {}"'.format(ffplay,hlsurl,model,stime,cserver,cgn)
             os.system(command)
             print(colored(" => END <= ", "yellow","on_blue"))
 
@@ -277,7 +278,7 @@ if __name__ == "__main__":
 
          if mod == 'URL':
             print (colored("\n => URL => {} <=", "white", "on_green")).format(fn4)
-            file=open(pf4,"wb")
+            file=open(pf4,'wb')
             file.write(hlsurl)
             file.close()
             raw_input(colored("\n => Press Enter to exit <=", "yellow", "on_blue"))
@@ -291,14 +292,14 @@ if __name__ == "__main__":
             sys.exit()
 
       else:
-         print (colored(" => ({}) is 'NO MOBILE FEED' model who isn't supported yet <=", "yellow", "on_red")).format(camgirl)
+         print (colored(" => ({}) is 'NO MOBILE FEED' model who isn't supported yet <=", "white", "on_red")).format(model)
          raw_input(colored("\n => Press Enter to exit <=", "yellow", "on_blue"))
          print(colored("\n => END <=", "yellow","on_blue"))
          time.sleep(1)
          sys.exit()
    else:
-      print (colored(" => ({}) video stream can't be recorded now <=", "yellow", "on_red")).format(camgirl)
-      raw_input(colored("\n => Press Enter to exit. <=", "yellow", "on_blue"))
+      print (colored(" => ({}) video stream can't be recorded now <=", "white", "on_red")).format(model)
+      raw_input(colored("\n => Press Enter to exit <=", "yellow", "on_blue"))
       print(colored("\n => END <=", "yellow","on_blue"))
       time.sleep(1)
       sys.exit()
