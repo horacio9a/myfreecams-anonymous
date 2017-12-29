ad08479c2c0dccd4a24af9ad2edca7ce33b69876
horacio9a <horacio9a@gmail.com>
2017-12-29 02:26:42 +0100

    v.1.0.8 Batch script for start-up all python scripts

--- mfc.bat
+++ mfc.bat
@@ -3,13 +3,13 @@ SETLOCAL EnableDelayedExpansion
 :START
 CLS
 ECHO.
-SET /P MODE=EXIT(6)  START(5)  MFCSLR(4)  MFCLSR(3)  MFCFFR(2)  MFCYTR(1)  MFC(0)(ENTER)(%MODE%): 
+SET /P MODE=EXIT(6)  START(5)  MFCYTR(4)  MFCSLR(3)  MFCLSR(2)  MFCFFR(1)  MFC(0)(ENTER)(%MODE%): 
 IF "%MODE%"=="" GOTO MFC
 IF "%MODE%"=="0" GOTO MFC
-IF "%MODE%"=="1" GOTO MFCYTR
-IF "%MODE%"=="2" GOTO MFCFFR
-IF "%MODE%"=="3" GOTO MFCLSR
-IF "%MODE%"=="4" GOTO MFCSLR
+IF "%MODE%"=="1" GOTO MFCFFR
+IF "%MODE%"=="2" GOTO MFCLSR
+IF "%MODE%"=="3" GOTO MFCSLR
+IF "%MODE%"=="4" GOTO MFCYTR
 IF "%MODE%"=="5" GOTO START
 IF "%MODE%"=="6" GOTO EXIT
 :MFC
@@ -24,39 +24,6 @@ start python mfc.py
 ECHO.
 PAUSE
 GOTO START
-:MFCYTR
-ECHO.
-SET n=0
-FOR /F "tokens=*" %%A IN (C:/Windows/MFC_Model.txt) DO (
-SET /A n=n+1
-SET _fav!n!=%%A
-ECHO !n! %%A
-)
-ECHO.
-SET /P MODEL=Choose MFC_ MODEL Name (%M% %MODEL%): 
-FOR /L %%f IN (1,1,!n!) DO (
-IF /I '%MODEL%'=='%%f' SET M=%%f
-)
-SET n=0
-FOR /F "tokens=*" %%A IN (C:/Windows/MFC_Model.txt) DO (
-SET /A n=n+1
-IF !n!==%M% SET MODEL=%%A
-)
-:MFCYTR_
-ECHO.
-SET MODELNAME=%MODEL% ###########################################
-SET _MODEL_=%MODELNAME:~0,37%
-ECHO.
-CLS && ECHO #####################################################
-ECHO ### MFCYTR ####  P Y T H O N   R E C  ###### 24/7 ###
-ECHO ### YTDL ###### %_MODEL_%
-ECHO #####################################################
-cd C:/
-COLOR 0F
-cd -mfc-py
-python mfcytr.py %MODEL%
-TIMEOUT 30
-GOTO MFCYTR_
 :MFCFFR
 ECHO.
 SET n=0
@@ -66,7 +33,7 @@ SET _fav!n!=%%A
 ECHO !n! %%A
 )
 ECHO.
-SET /P MODEL=Choose MFC_ MODEL Name (%M% %MODEL%): 
+SET /P MODEL=Choose MFC Model Name (%M% %MODEL%): 
 FOR /L %%f IN (1,1,!n!) DO (
 IF /I '%MODEL%'=='%%f' SET M=%%f
 )
@@ -77,7 +44,7 @@ IF !n!==%M% SET MODEL=%%A
 )
 :MFCFFR_
 ECHO.
-SET MODELNAME=%MODEL% ###########################################
+SET MODELNAME=%MODEL% ############## %M% ##########################
 SET _MODEL_=%MODELNAME:~0,37%
 ECHO.
 CLS && ECHO #####################################################
@@ -99,7 +66,7 @@ SET _fav!n!=%%A
 ECHO !n! %%A
 )
 ECHO.
-SET /P MODEL=Choose MFC MODEL Name (%M% %MODEL%): 
+SET /P MODEL=Choose MFC Model Name (%M% %MODEL%): 
 FOR /L %%f IN (1,1,!n!) DO (
 IF /I '%MODEL%'=='%%f' SET M=%%f
 )
@@ -110,7 +77,7 @@ IF !n!==%M% SET MODEL=%%A
 )
 :MFCLSR_
 ECHO.
-SET MODELNAME=%MODEL% ############################################
+SET MODELNAME=%MODEL% ############## %M% ##########################
 SET _MODEL_=%MODELNAME:~0,34%
 ECHO.
 CLS && ECHO #################################################
@@ -132,7 +99,7 @@ SET _fav!n!=%%A
 ECHO !n! %%A
 )
 ECHO.
-SET /P MODEL=Choose MFC MODEL Name (%M% %MODEL%): 
+SET /P MODEL=Choose MFC Model Name (%M% %MODEL%): 
 FOR /L %%f IN (1,1,!n!) DO (
 IF /I '%MODEL%'=='%%f' SET M=%%f
 )
@@ -143,7 +110,7 @@ IF !n!==%M% SET MODEL=%%A
 )
 :MFCSLR_
 ECHO.
-SET MODELNAME=%MODEL% #######################################
+SET MODELNAME=%MODEL% ############## %M% ##########################
 SET _MODEL_=%MODELNAME:~0,33%
 ECHO.
 CLS && ECHO #################################################
@@ -156,6 +123,39 @@ cd -mfc-py
 python mfcslr.py %MODEL%
 TIMEOUT 30
 GOTO MFCSLR_
+:MFCYTR
+ECHO.
+SET n=0
+FOR /F "tokens=*" %%A IN (C:/Windows/MFC_Model.txt) DO (
+SET /A n=n+1
+SET _fav!n!=%%A
+ECHO !n! %%A
+)
+ECHO.
+SET /P MODEL=Choose MFC Model Name (%M% %MODEL%): 
+FOR /L %%f IN (1,1,!n!) DO (
+IF /I '%MODEL%'=='%%f' SET M=%%f
+)
+SET n=0
+FOR /F "tokens=*" %%A IN (C:/Windows/MFC_Model.txt) DO (
+SET /A n=n+1
+IF !n!==%M% SET MODEL=%%A
+)
+:MFCYTR_
+ECHO.
+SET MODELNAME=%MODEL% ############## %M% ##########################
+SET _MODEL_=%MODELNAME:~0,37%
+ECHO.
+CLS && ECHO #####################################################
+ECHO ### MFCYTR ####  P Y T H O N   R E C  ###### 24/7 ###
+ECHO ### YTDL ###### %_MODEL_%
+ECHO #####################################################
+cd C:/
+COLOR 0F
+cd -mfc-py
+python mfcytr.py %MODEL%
+TIMEOUT 30
+GOTO MFCYTR_
 :EXIT
 GOTO :EOF
 ENDLOCAL
