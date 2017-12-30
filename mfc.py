93ef34e6decb6d8cc1da5d03daf9b00c51730147
horacio9a <horacio9a@gmail.com>
2017-12-30 09:38:01 +0100

    v.1.0.9 Basic myfreecams-anonymous all modes script with proxy

--- mfc.py
+++ mfc.py
@@ -1,9 +1,8 @@
-# MFC Anonymous All Modes Recorder v.1.0.8 by horacio9a for Python 2.7.14
+# MFC Anonymous All Modes Recorder v.1.0.9 by horacio9a for Python 2.7.14
 
 import sys,os,urllib,re,json,time,datetime,random,requests,command,websocket
 reload(sys)
 sys.setdefaultencoding('utf-8')
-from websocket import create_connection
 from colorama import init, Fore, Back, Style
 from termcolor import colored
 import ConfigParser
@@ -11,9 +10,7 @@ config = ConfigParser.ConfigParser()
 config.read('config.cfg')
 
 init()
-print
-print(colored(" => START <=", "yellow", "on_blue"))
-print
+print(colored("\n => START <=", "yellow", "on_blue"))
 
 vs_str = {}
 vs_str[0] = 'PUBLIC'
@@ -27,9 +24,9 @@ def fc_decode_json(m):
    try:
       return json.loads(m[m.find('{'):].decode('utf-8','ignore'))
    except:
-      print (colored(" => Error reply ... check your entry ({}) <=\n", "white", "on_red")).format(model)
+      print (colored("\n => Error reply ... check your entry ({}) <=", "white", "on_red")).format(model)
       time.sleep(6)
-      print(colored(" => END <=", "yellow","on_blue"))
+      print(colored("\n => END <=", "yellow","on_blue"))
       time.sleep(1)
       sys.exit()
 
@@ -43,9 +40,9 @@ def read_model_data(m):
    cid = msg['uid'] + 100000000
    vs = msg['vs']
    if vs == 127:
-      print (colored(" => Model ({}) is OFFLINE <=\n", "white", "on_red")).format(model)
+      print (colored("\n => Model ({}) is OFFLINE <=", "white", "on_red")).format(model)
       time.sleep(3)
-      print(colored(" => END <=", 'yellow','on_blue'))
+      print(colored("\n => END <=", 'yellow','on_blue'))
       time.sleep(1)
       sys.exit()
    m_info = msg['m']
@@ -112,11 +109,11 @@ def read_model_data(m):
    except:
       pass
 
-   print (colored(" => ({}) * {} * ({}) * Server: {} * Flags: {} * Score: {} <=", "white", "on_blue")).format(model,buf,cserver,server,flags,camscore)
+   print (colored("\n => ({}) * {} * ({}) * Server: {} * Flags: {} * Score: {} <=", "white", "on_blue")).format(model,buf,cserver,server,flags,camscore)
    print (colored("\n => Continent: {} * Location: {}-{} * Age: {} * Ethnic: {} <=", "yellow", "on_blue")).format(continent,city,country,age,ethnic)
    print (colored("\n => Occupation: {} * New: {} * Viewers: {} * Blurb: {} <=", "yellow", "on_blue")).format(occupation,newmodel,rc,blurb)
-   print (colored("\n => Topic => {} <=\n", "blue", "on_white")).format(topic)
-   # print (colored(" => (MODEL DATA) => {} <=\n", "white", "on_blue")).format(mdata)
+   print (colored("\n => Topic => {} <=", "blue", "on_white")).format(topic)
+   # print (colored("\n => (MODEL DATA) => {} <=", "white", "on_blue")).format(mdata)
 
 if __name__ == '__main__':
    if len(sys.argv) > 1:
@@ -129,12 +126,9 @@ if __name__ == '__main__':
                print " =>",(num+1),value[:-1]
             print
             cgn = int(raw_input(colored(" => Select MFC Model => ", "yellow", "on_blue")))
-            print
             break
          except ValueError:
-            print
-            print(colored(" => Input must be a number <=", "white", "on_red"))
-            print
+            print(colored("\n => Input must be a number <=", "white", "on_red"))
       model = open(config.get('files', 'model_list'), 'r').readlines()[cgn-1][:-1]
    try:
       cservers = ["xchat107","xchat108","xchat61","xchat94","xchat109","xchat22","xchat47","xchat48","xchat49",
@@ -147,29 +141,29 @@ if __name__ == '__main__':
                  "xchat34","xchat35","xchat36","xchat90","xchat92","xchat93","xchat81","xchat83","xchat79","xchat68",
                  "xchat78","xchat84","xchat85","xchat86","xchat87","xchat88","xchat89","xchat96","xchat97","xchat98",
                  "xchat99","xchat100","xchat101","xchat102","xchat103","xchat104","xchat105","xchat106","xchat127"];
-      # https://www.myfreecams.com/_js/serverconfig.js
       cserver = '{}'.format(random.choice(cservers))
-      ws_host = 'wss://{}.myfreecams.com/fcsl'.format(cserver)
-      ws = create_connection(ws_host)
-      # https://www.myfreecams.com/js/wsgw.js
-      # https://www.myfreecams.com/js/FCS.js
+      ws_host = 'ws://{}.myfreecams.com:8080/fcsl'.format(cserver)
+      ws = websocket.WebSocket()
+      ws.connect(ws_host, sslopt={"check_hostname": False})
+      # ws.connect(ws_host, http_proxy_host='159.203.126.11', http_proxy_port=8080, sslopt={"check_hostname": False})
+
       send_msg_hello = 'hello fcserver\n\0'
-      # FCTYPE_LOGIN = 1
       send_msg_login = '1 0 0 20071025 0 guest:guest\n\0'
-      # FCTYPE_PING = 1
       send_msg_ping = '1 0 0 0 0\n\0'
-      # FCTYPE_MODEL = 10
+      send_msg_keep_alive = '0 0 0 1 0\n\0'
       send_msg_model = '10 0 0 20 0 {}\n\0'.format(model)
-      # FCTYPE_LOGOUT = 99
       send_msg_logout = '99 0 0 0 0'
+
       ws.send(send_msg_hello)
       ws.send(send_msg_login)
+
    except:
-      print (colored(" => {} server is busy ... Try again <=\n", "white", "on_red")).format(cserver)
+      print (colored("\n => {} server is busy ... Try again <=", "white", "on_red")).format(cserver)
       time.sleep(3)
-      print(colored(" => END <=", "yellow","on_blue"))
+      print(colored("\n => END <=", "yellow","on_blue"))
       time.sleep(1)
       sys.exit()
+
    rembuf = ''
    quitting = 0
    while quitting == 0:
@@ -196,17 +190,17 @@ if __name__ == '__main__':
          sock_buf = sock_buf[4+mlen:]
          if len(sock_buf) == 0:
             break
-   ws.send(send_msg_logout)
+   # ws.send(send_msg_logout)
    ws.close()
 
    if vs == 0:
       if server != 0:
          while True:
              try:
-                mode = int(raw_input(colored(" => Mode => Exit(6) => URL(5) => YTDL(4) => SL(3) => LS(2) => FFMPEG(1) => FFPLAY(0) => ", "white", "on_green")))
+                mode = int(raw_input(colored("\n => Mode => Exit(6) => URL(5) => YTDL(4) => SL(3) => LS(2) => FFMPEG(1) => FFPLAY(0) => ", "white", "on_green")))
                 break
              except ValueError:
-                print(colored("\n => Input must be a number <=\n", "white", "on_red"))
+                print(colored("\n => Input must be a number <=", "white", "on_red"))
          if mode == 0:
             mod = 'FFPLAY'
          if mode == 1:
@@ -290,14 +284,14 @@ if __name__ == '__main__':
             sys.exit()
 
       else:
-         print (colored(" => ({}) is 'NO MOBILE FEED' model who isn't supported yet <=", "white", "on_red")).format(model)
+         print (colored("\n => ({}) is 'NO MOBILE FEED' model who isn't supported yet <=", "white", "on_red")).format(model)
          raw_input(colored("\n => Press Enter to exit <=", "yellow", "on_blue"))
          print(colored("\n => END <=", "yellow","on_blue"))
          time.sleep(1)
          sys.exit()
 
    else:
-      print (colored(" => ({}) video stream can't be recorded now <=", "white", "on_red")).format(model)
+      print (colored("\n => ({}) video stream can't be recorded now <=", "white", "on_red")).format(model)
       raw_input(colored("\n => Press Enter to exit <=", "yellow", "on_blue"))
       print(colored("\n => END <=", "yellow","on_blue"))
       time.sleep(1)
