5971775e122dcbdabb5116bf67aa74ed28a5f476
horacio9a <horacio9a@gmail.com>
2018-03-01 03:01:55 +0100

    v.1.0.10 Batch script for start-up all python scripts
    added rtmp and empty files delete

--- mfc.bat
+++ mfc.bat
@@ -1,17 +1,19 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
 :START
+SET OUT_DIR=C:/Videos/MFC/
 CLS
 ECHO.
-SET /P MODE=EXIT(6)  START(5)  MFCYTR(4)  MFCSLR(3)  MFCLSR(2)  MFCFFR(1)  MFC(0)(ENTER)(%MODE%): 
+SET /P MODE=EX(7) START(6) MFCYTR(5) MFCSLR(4) MFCLSR(3) MFCFFR(2) MFCRTMPR(1) MFC(0)(%MODE%): 
 IF "%MODE%"=="" GOTO MFC
 IF "%MODE%"=="0" GOTO MFC
-IF "%MODE%"=="1" GOTO MFCFFR
-IF "%MODE%"=="2" GOTO MFCLSR
-IF "%MODE%"=="3" GOTO MFCSLR
-IF "%MODE%"=="4" GOTO MFCYTR
-IF "%MODE%"=="5" GOTO START
-IF "%MODE%"=="6" GOTO EXIT
+IF "%MODE%"=="1" GOTO MFCRTMPR
+IF "%MODE%"=="2" GOTO MFCFFR
+IF "%MODE%"=="3" GOTO MFCLSR
+IF "%MODE%"=="4" GOTO MFCSLR
+IF "%MODE%"=="5" GOTO MFCYTR
+IF "%MODE%"=="6" GOTO START
+IF "%MODE%"=="7" GOTO EXIT
 :MFC
 ECHO.
 CLS && ECHO #################################################
@@ -24,6 +26,41 @@ start python mfc.py
 ECHO.
 PAUSE
 GOTO START
+:MFCRTMPR
+ECHO.
+SET n=0
+FOR /F "tokens=*" %%A IN (C:/Windows/MFC_Model.txt) DO (
+SET /A n=n+1
+SET _fav!n!=%%A
+ECHO !n! %%A
+)
+ECHO.
+SET /P MODEL=Choose MFC Model Name (%M% %MODEL%): 
+FOR /L %%f IN (1,1,!n!) DO (
+IF /I '%MODEL%'=='%%f' SET M=%%f
+)
+SET n=0
+FOR /F "tokens=*" %%A IN (C:/Windows/MFC_Model.txt) DO (
+SET /A n=n+1
+IF !n!==%M% SET MODEL=%%A
+)
+:MFCRTMPR_
+COLOR 0F
+ECHO.
+SET MODELNAME=%MODEL% ############ %M% ############################
+SET _MODEL_=%MODELNAME:~0,33%
+ECHO.
+CLS && ECHO #################################################
+ECHO ### MFCRTMPR ## P Y T H O N   R E C #### 24/7 ###
+ECHO ### RTMP ###### %_MODEL_%
+ECHO #################################################
+cd C:/-mfc-py
+python mfcrtmpr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
+TIMEOUT 30
+GOTO MFCRTMPR_
+
 :MFCFFR
 ECHO.
 SET n=0
@@ -43,6 +80,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :MFCFFR_
+COLOR 0F
 ECHO.
 SET MODELNAME=%MODEL% ############ %M% ############################
 SET _MODEL_=%MODELNAME:~0,33%
@@ -51,10 +89,10 @@ CLS && ECHO #################################################
 ECHO ### MFCFFR #### P Y T H O N   R E C #### 24/7 ###
 ECHO ### FFMPEG #### %_MODEL_%
 ECHO #################################################
-cd C:/
-COLOR 0F
-cd -mfc-py
+cd C:/-mfc-py
 python mfcffr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
 TIMEOUT 30
 GOTO MFCFFR_
 :MFCLSR
@@ -76,6 +114,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :MFCLSR_
+COLOR 0F
 ECHO.
 SET MODELNAME=%MODEL% ############ %M% ############################
 SET _MODEL_=%MODELNAME:~0,33%
@@ -84,10 +123,10 @@ CLS && ECHO ################################################
 ECHO ### MFCLSR ### P Y T H O N   R E C #### 24/7 ###
 ECHO ### LS ####### %_MODEL_%
 ECHO ################################################
-cd C:/
-COLOR 0F
-cd -mfc-py
+cd C:/-mfc-py
 python mfclsr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
 TIMEOUT 30
 GOTO MFCLSR_
 :MFCSLR
@@ -109,6 +148,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :MFCSLR_
+COLOR 0F
 ECHO.
 SET MODELNAME=%MODEL% ############ %M% ############################
 SET _MODEL_=%MODELNAME:~0,33%
@@ -117,10 +157,10 @@ CLS && ECHO #################################################
 ECHO ### MFCSLR #### P Y T H O N   R E C #### 24/7 ###
 ECHO ### SL ######## %_MODEL_%
 ECHO #################################################
-cd C:/
-COLOR 0F
-cd -mfc-py
+cd C:/-mfc-py
 python mfcslr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
 TIMEOUT 30
 GOTO MFCSLR_
 :MFCYTR
@@ -142,6 +182,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :MFCYTR_
+COLOR 0F
 ECHO.
 SET MODELNAME=%MODEL% ############ %M% ############################
 SET _MODEL_=%MODELNAME:~0,33%
@@ -150,10 +191,10 @@ CLS && ECHO #################################################
 ECHO ### MFCYTR #### P Y T H O N   R E C #### 24/7 ###
 ECHO ### YTDL ###### %_MODEL_%
 ECHO #################################################
-cd C:/
-COLOR 0F
-cd -mfc-py
+cd C:/-mfc-py
 python mfcytr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
 TIMEOUT 30
 GOTO MFCYTR_
 :EXIT
